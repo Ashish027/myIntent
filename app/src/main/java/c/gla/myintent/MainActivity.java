614c8a8395096cc5479bdab7f85194be0ee01dde
Ashish Maurya <ashish027>
2019-02-28 15:42:10 +0530

    Main activity layout created Main activity home page done home page created  commit

---
+++ app/src/main/java/c/gla/myintent/MainActivity.java
@@ -0,0 +1,20 @@
+package c.gla.myintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void myHome(View view) {
+        Intent i = new Intent(this,home.class);
+        startActivity(i);
+    }
+}
